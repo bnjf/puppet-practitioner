e7d57e423626345aaf2c49b22b5b35c93e9bb739
brad <brad@puppetlabs.vm>
2016-03-23 00:09:07 +0000

    exporting a resource

---
+++ modules/hosts/manifests/init.pp
@@ -0,0 +1,15 @@
+class hosts {
+
+  resources { 'host':
+    purge => true,
+  }
+
+  @@host { $::hostname:
+    ip           => $::ipaddress,
+    host_aliases => $::fqdn,
+    tag          => 'classroom',
+  }
+
+  Host <<| $tag == 'classroom' |>>
+
+}
