f08626c344daeabaaa01a537c01c3539c113c99f
brad <brad@puppetlabs.vm>
2016-03-23 00:16:59 +0000

    need the local defaults too

--- modules/hosts/manifests/init.pp
+++ modules/hosts/manifests/init.pp
@@ -12,4 +12,30 @@ class hosts {
 
   Host <<| tag == 'classroom' |>>
 
+
+  host { 'brad.puppetlabs.vm':
+    ensure       => 'present',
+    host_aliases => ['brad'],
+    ip           => '10.0.20.28',
+    target       => '/etc/hosts',
+  }
+  host { 'localhost':
+    ensure       => 'present',
+    host_aliases => ['localhost.localdomain', 'localhost6', 'localhost6.localdomain6'],
+    ip           => '::1',
+    target       => '/etc/hosts',
+  }
+  host { 'master.puppetlabs.vm':
+    ensure       => 'present',
+    host_aliases => ['master'],
+    ip           => '10.0.20.5',
+    target       => '/etc/hosts',
+  }
+  host { 'student.puppetlabs.vm':
+    ensure       => 'present',
+    host_aliases => ['training', 'localhost', 'localhost.localdomain', 'localhost4'],
+    ip           => '127.0.0.1',
+    target       => '/etc/hosts',
+  }
+
 }
