d7f4622a77cd1f0aaaa503c1ce3d7ebaf8530d40
brad <brad@puppetlabs.vm>
2016-03-23 01:18:21 +0000

    fix

--- modules/kerberos/lib/facter/default_realm.rb
+++ modules/kerberos/lib/facter/default_realm.rb
@@ -2,14 +2,15 @@
 
 Facter.add('default_realm') do
   setcode do
+    default_realm = nil
     File.open('/etc/krb5.conf') do |f1|
       while line = f1.gets
         if m = line.match('^\s*\bdefault_realm\b\s*=\s*(\S+)') then
           default_realm = m[1]
-          break
         end
       end
     end
+    default_realm
   end
 end
 
