52914ab6cc7504c31ac88ec26eb8cc717c1a7356
brad <brad@puppetlabs.vm>
2016-03-23 01:15:44 +0000

    newfunction to add functions (duh)

--- modules/homedir/lib/puppet/parser/functions/homedir.rb
+++ modules/homedir/lib/puppet/parser/functions/homedir.rb
@@ -1,11 +1,12 @@
 #!/usr/bin/env ruby
 
 module Puppet::Parser::Functions
-  homedir(:username,
+  newfunction(:homedir,
           :type => :rvalue,
           :arity => 1
          ) do |args|
            raise ArgumentError, 'Expects a string' unless args.first.class == String
+           username = args.first
            if username == "root" then
              return "/root"
            else
